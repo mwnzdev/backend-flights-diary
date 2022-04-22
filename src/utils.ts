50fac6de528bff657183568c38e369e3c6bdd430
Michelle <mw.devnz@gmail.com>
2022-04-22 23:50:09 +1200

    added parsing for SSN

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import { NewPatient } from "./types";
 const toNewPatient = (object: unknown): NewPatient => {
 	const newEntry: NewPatient = {
 		name: parseName(object.name),
-		dataOfBirth: parseDataOfBirth(object.dateOfBirth),
+		dateOfBirth: parseDateOfBirth(object.dateOfBirth),
 		ssn: parseSsn(object.ssn),
 		gender: parseGender(object.gender),
 		occupation: parseOccupation(object.occupation)
@@ -15,7 +15,7 @@ export default toNewPatient;
 
 const parseName = (name: unknown): string => {
 	if (!name || !isString(name)) {
-		throw new Error('Incorrect or missing comment');
+		throw new Error('Incorrect or missing name: ' + name);
 	}
 	return name;
 };
@@ -24,4 +24,21 @@ const isString = (text: unknown): text is string => {
 	return typeof text === 'string' || text instanceof String;
 };
 
+const parseDateOfBirth = (dateOfBirth: unknown): string => {
+	if (!dateOfBirth || !isString(dateOfBirth) || isDate(dateOfBirth)) {
+		throw new Error('Incorrect or missing date: ' + dateOfBirth);
+	}
+	return dateOfBirth;
+};
+
+const isDate = (date: string) => {
+	return Boolean(Date.parse(date));
+};
+
+const parseSsn = (ssn: unknown): string => {
+	if (!ssn || !isString(ssn)) {
+		throw new Error('Incorrect or missing ssn: ' + ssn);
+	}
+	return ssn;
+};
 
